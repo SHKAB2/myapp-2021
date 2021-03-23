46d3450940d519cd894c6723af8df35d66922b88
SHKAB2 <shkab786@gmail.com>
2021-03-23 16:26:36 +0530

    git demo9

---
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -0,0 +1,20 @@
+package in.javahome.myweb.controller;
+import java.util.List;
+/*
+ * 
+ */
+public class Calculator {
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		String s1 = null;
+		s1.toString();
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+}
